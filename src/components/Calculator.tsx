62c0e9bc930029ed49a86f5d270690c87df9cc62
Pranav Maheshwari <pmaheshwari@deqode.com>
2021-07-30 12:59:24 +0530

    Update Calculator | Added hooks

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import Display from './Display';
 
@@ -7,16 +7,54 @@ const buttonList = [
     '7', '8', '9', '×',
     '4', '5', '6', '-',
     '1', '2', '3', '+',
-    '0', '.', '='];
-
-const handleButtonClick = (e: Event) => {
-    console.log('Button Pressed', e.target);
-}
+    '0', '.', '='
+];
 
 const Calculator = () => {
+    const [output, setOutput] = useState('0');
+    const [input, setInput] = useState('');
+    const [operator, setOperator] = useState('');
+
+    const handleButtonClick = (inputValue: string) => {
+        if (inputValue === buttonList[0]) {
+            setOutput('0');
+        }
+        else {
+            // Add if number
+            if (/\d/g.test(inputValue)) {
+                if (operator) {
+                    setOutput(inputValue);
+                    const prevValue = parseFloat(input);
+                    const nextValue = parseFloat(input);
+
+                    const newValue: number = performCalculation[operator](prevValue, nextValue);
+
+                    setOutput(String(newValue));
+                    setInput(String(newValue));
+                    setOperator('');
+                } else {
+                    setInput(input + inputValue);
+                    setOutput(output + inputValue);
+                }
+            } else {
+                setOperator(inputValue);
+            }
+            console.log('Button Pressed', inputValue);
+        }
+    }
+
+    const performCalculation: any = {
+        '/': (previousValue: number, nextValue: number) => previousValue / nextValue,
+        '*': (previousValue: number, nextValue: number) => previousValue * nextValue,
+        '-': (previousValue: number, nextValue: number) => previousValue - nextValue,
+        '+': (previousValue: number, nextValue: number) => previousValue + nextValue,
+        '=': (previousValue: number, nextValue: number) => nextValue
+    }
+
+
     return (
         <>
-            <Display valueToShow="0" />
+            <Display valueToShow={output} />
 
             {
                 buttonList.map((value: string, idx: number) => {
