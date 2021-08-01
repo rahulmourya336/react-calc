bcfec638f6a137af51b7e289759431b7c0705728
rahulmourya336@gmail.com <rahul@yahoo.com>
2021-08-01 22:11:47 +0530

    Add operator UI & styles

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,7 +1,26 @@
 import { FC } from 'react';
+import { INFINITY } from '../Models/Calculator';
 import { DisplayPropType } from '../Models/Display';
-import { Screen, ScreenWrapper } from '../styles';
+import { Operator, Screen, ScreenWrapper } from '../styles';
 
-const Display: FC<DisplayPropType> = ({ valueToShow }) => <ScreenWrapper><Screen>  {valueToShow}  </Screen></ScreenWrapper>
+const Display: FC<DisplayPropType> = ({ valueToShow, operator }) =>
+    <ScreenWrapper><Screen>  {formatValue(valueToShow)} <Operator>{operator}</Operator></Screen></ScreenWrapper>
 
+const formatValue = (value: string) => {
+    if (value === INFINITY) return value;
+    const language = navigator.language || 'en-US'
+    let formattedValue = parseFloat(value).toLocaleString(language, {
+        useGrouping: true,
+        maximumFractionDigits: 6
+    })
+
+    // Add back missing .0 in e.g. 12.0
+    const match = value.match(/\.\d*?(0*)$/);
+
+    if (match) {
+        formattedValue += (/[1-9]/).test(match[0]) ? match[1] : match[0];
+    }
+
+    return formattedValue;
+}
 export default Display;
--- src/styles.tsx
+++ src/styles.tsx
@@ -6,6 +6,7 @@ const orangeColor = '#ff8585';
 const greenColor = '#43ffdc';
 const bgDark = '#2e323c';
 const bgMinimumDark = '#292d36';
+const operatorDisplayColor = '#9ea2ac';
 
 export const Wrapper = styled.div`
 width: 290px;
@@ -15,6 +16,7 @@ display: flex;
 flex-wrap:wrap;
 border-radius: 20px;
 border: 1px solid lightgrey;
+background: ${bgMinimumDark};
 `;
 
 export const Screen = styled.div`
@@ -28,6 +30,16 @@ color: #fff;
 border-radius: 5px 5px 0 0;
 padding: 45px 10px 0 10px;
 margin: 0px;
+height: 135px;
+`;
+
+export const Operator = styled.span`
+display: block;
+padding: 0;
+margin: 0;
+line-height: 1.5;
+font-size: smaller;
+color: ${operatorDisplayColor};
 `;
 
 export const ScreenWrapper = styled.div`
@@ -46,11 +58,12 @@ border: none;
 font-size: x-large;
 cursor: pointer;
 outline: 0;
-background: #292D36;
-color: ${props => props.className === 'orange' ? orangeColor : props.className === 'green' ? greenColor : buttonColor};
-flex-basis: ${props => props.className === 'long-btn' ? '40%' : 'none' };
+background: ${bgMinimumDark};
+color: ${props => props.className.includes('orange') ? orangeColor : props.className.includes('green') ? greenColor : buttonColor};
+flex-basis: ${props => props.className.includes('long-btn') ? '40%' : 'none' };
 &:active {
-    box-shadow: inset 0px 0px 5px ${buttonActiveColor};
+    box-shadow: inset 20px 20px 15px ${buttonActiveColor};
+    opacity: 0.6;
 }
 `;
 
