0897e25ba6e517e822db39c77cce1dcc7684ffb6
rahulmourya336@gmail.com <rahul@yahoo.com>
2021-08-02 13:06:03 +0530

    Update operate.tsx: Code format

--- src/Logic/operate.tsx
+++ src/Logic/operate.tsx
@@ -1,9 +1,16 @@
 import Big from "big.js";
-import { INFINITY } from "../Models/Calculator";
+import { INFINITY } from "../models/calculator";
 
-export default function operate(numberOne: string, numberTwo: string, operation: string) {
+export default function operate(
+  numberOne: string,
+  numberTwo: string,
+  operation: string
+) {
   const one = Big(numberOne || "0");
-  const two: any = Big(numberTwo || (operation === "/" || operation === '*' ? "1": "0")); //If dividing or multiplying, then 1 maintains current value in cases of null
+  //If dividing or multiplying, then 1 maintains current value in cases of null
+  const two: any = Big(
+    numberTwo || (operation === "/" || operation === "*" ? "1" : "0")
+  );
   if (operation === "+") {
     return one.plus(two).toString();
   }
@@ -14,7 +21,7 @@ export default function operate(numberOne: string, numberTwo: string, operation:
     return one.times(two).toString();
   }
   if (operation === "/") {
-    if (String(two['c'][0]) === "0") {
+    if (String(two["c"][0]) === "0") {
       return String(INFINITY);
     } else {
       return one.div(two).toString();
