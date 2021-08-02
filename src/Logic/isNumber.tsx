cca0d71632005c8c749a8d5b7c8f7dfd4ff243a3
rahulmourya336@gmail.com <rahul@yahoo.com>
2021-08-02 13:01:48 +0530

    Update isNumber to utils

--- src/Logic/isNumber.tsx
+++
@@ -1,3 +0,0 @@
-export default function isNumber(item: string) {
-  return /[0-9]+/.test(item);
-}
---
+++ src/constants/index.tsx
@@ -0,0 +1,99 @@
+import { ButtonListType } from "../models/button";
+
+export const buttonList: Array<ButtonListType> = [
+  {
+    name: "AC",
+    value: "AC",
+    classes: "green",
+  },
+  {
+    name: "±",
+    value: "±",
+    classes: "green",
+  },
+  {
+    name: "%",
+    value: "%",
+    classes: "green",
+  },
+  {
+    name: "÷",
+    value: "/",
+    classes: "orange",
+  },
+  {
+    name: "7",
+    value: "7",
+    classes: "",
+  },
+  {
+    name: "8",
+    value: "8",
+    classes: "",
+  },
+  {
+    name: "9",
+    value: "9",
+    classes: "",
+  },
+  {
+    name: "×",
+    value: "*",
+    classes: "orange",
+  },
+  {
+    name: "4",
+    value: "4",
+    classes: "",
+  },
+  {
+    name: "5",
+    value: "5",
+    classes: "",
+  },
+  {
+    name: "6",
+    value: "6",
+    classes: "",
+  },
+  {
+    name: "-",
+    value: "-",
+    classes: "orange",
+  },
+  {
+    name: "1",
+    value: "1",
+    classes: "",
+  },
+  {
+    name: "2",
+    value: "2",
+    classes: "",
+  },
+  {
+    name: "3",
+    value: "3",
+    classes: "",
+  },
+  {
+    name: "+",
+    value: "+",
+    classes: "orange",
+  },
+  {
+    name: "0",
+    value: "0",
+    classes: "",
+  },
+  {
+    name: ".",
+    value: ".",
+    classes: "",
+  },
+  {
+    name: "=",
+    value: "=",
+    classes: "orange long-btn",
+  },
+];
