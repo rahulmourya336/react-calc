f6eee5d6b4abccdc170fd31fa501d23618efcfa4
rahulmourya336@gmail.com <rahul@yahoo.com>
2021-08-02 13:04:40 +0530

    Update calculate.tsx: Code format & floating point fix

--- src/Logic/calculate.tsx
+++ src/Logic/calculate.tsx
@@ -1,8 +1,8 @@
 import Big from "big.js";
 
 import operate from "./operate";
-import isNumber from "./isNumber";
-import { CalcPropType } from "../Models/Calculator";
+import isNumber from "../utils/isNumber";
+import { CalcPropType } from "../models/calculator";
 
 /**
  * Given a button name and a calculator data object, return an updated
@@ -18,7 +18,7 @@ const defaultReturn: CalcPropType = {
   total: null,
   next: null,
   operation: null,
-}
+};
 
 export default function calculate(obj: CalcPropType, buttonName: string) {
   if (buttonName === "AC") {
@@ -27,15 +27,16 @@ export default function calculate(obj: CalcPropType, buttonName: string) {
 
   if (isNumber(buttonName)) {
     if (buttonName === "0" && obj.next === "0") {
-      // return {};
-      return defaultReturn;
+      return {};
     }
+
     // If there is an operation, update next
     if (obj.operation) {
-      if (obj.next) {
-        return { next: obj.next + buttonName, total: obj.total, operation: obj.operation };
-      }
-      return { next: buttonName, total: obj.total, operation: obj.operation };
+      const next = obj.next ? obj.next + buttonName : buttonName;
+      return {
+        ...obj,
+        next,
+      };
     }
     // If there is no operation, update next and clear the value
     if (obj.next) {
@@ -43,13 +44,13 @@ export default function calculate(obj: CalcPropType, buttonName: string) {
       return {
         next,
         total: null,
-        operation: obj.operation
+        operation: obj.operation,
       };
     }
     return {
       next: buttonName,
       total: null,
-      operation: obj.operation
+      operation: obj.operation,
     };
   }
 
@@ -57,20 +58,15 @@ export default function calculate(obj: CalcPropType, buttonName: string) {
     if (obj.operation && obj.next) {
       const result = operate(obj.total, obj.next, obj.operation);
       return {
-        total: Big(result)
-          .div(Big("100"))
-          .toString(),
+        total: Big(result).div(Big("100")).toString(),
         next: null,
         operation: null,
       };
     }
     if (obj.next) {
       return {
-        next: Big(obj.next)
-          .div(Big("100"))
-          .toString(),
-        total: obj.total,
-        operation: obj.operation,
+        ...obj,
+        next: Big(obj.next).div(Big("100")).toString(),
       };
     }
     return {};
@@ -82,9 +78,9 @@ export default function calculate(obj: CalcPropType, buttonName: string) {
       if (obj.next.includes(".")) {
         return {};
       }
-      return { next: obj.next + "." };
+      return { ...obj, next: obj.next + "." };
     }
-    return { next: "0." };
+    return { ...obj, next: "0." };
   }
 
   if (buttonName === "=") {
@@ -95,28 +91,25 @@ export default function calculate(obj: CalcPropType, buttonName: string) {
         operation: null,
       };
     } else {
-      // '=' with no operation, nothing to do
       return {};
     }
   }
 
   if (buttonName === "±") {
     if (obj.next) {
-      return { next: (-1 * parseFloat(obj.next)).toString(), total: obj.total, operation: obj.operation };
+      return {
+        ...obj,
+        next: (-1 * parseFloat(obj.next)).toString(),
+      };
     }
     if (obj.total) {
-      return { total: (-1 * parseFloat(obj.total)).toString(), next: obj.next, operation: obj.operation };
+      return {
+        ...obj,
+        total: (-1 * parseFloat(obj.total)).toString(),
+      };
     }
     return {};
   }
-
-  // Button must be an operation
-
-  // When the user presses an operation button without having entered
-  // a number first, do nothing.
-  // if (!obj.next && !obj.total) {
-  //   return {};
-  // }
 
   // User pressed an operation button and there is an existing operation
   if (obj.operation) {
@@ -126,8 +119,6 @@ export default function calculate(obj: CalcPropType, buttonName: string) {
       operation: buttonName,
     };
   }
-
-  // no operation yet, but the user typed one
 
   // The user hasn't typed a number yet, just save the operation
   if (!obj.next) {
